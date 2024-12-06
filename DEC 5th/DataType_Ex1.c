1145a75afb3d8af043ea86203cbd0c635fd3dfba
chandraParvam <133752010+chandraParvam@users.noreply.github.com>
2024-12-06 11:17:18 +0530

    first commit

---
+++ DEC 5th/DataType_Ex1.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    int val1,val2;
+    printf("Please Enter Two Number to Perform Addition:");
+    scanf("%d%d",&val1,&val2);
+    printf(" sum of two numbers %d + %d = %d",val1,val2,val1+val2);
+    return 0;
+}
---
+++ DEC 6th/ternaryOp.c
@@ -0,0 +1,17 @@
+// find the number even or odd
+// check voters
+// greater than 18 
+
+#include<stdio.h>
+int main(){
+   int number=0;
+   printf("Enter the to find even or odd : ");
+   scanf("%d",&number);
+   (number%2 == 0) ? printf("it is Even") : printf("it is Odd");
+        // if(number%2 == 0 ){
+        //     printf("it is Even");
+        // }else{
+        //     printf("it is Odd");
+        // }
+    return 0;
+}
